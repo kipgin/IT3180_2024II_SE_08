50dbf5ed09441828c51cef08f936fecb5ef6e19c
Cao Huy Thịnh <149091575+caohuythinh2005@users.noreply.github.com>
2025-03-07 00:04:35 +0700

    test2

--- src/main/java/com/example/BTL_CNPM/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/BTL_CNPM/service/impl/AuthServiceImpl.java
@@ -1,29 +1,29 @@
-package com.example.BTL_CNPM.service.impl;
-
-import com.example.BTL_CNPM.model.LoginRequest;
-import com.example.BTL_CNPM.model.User;
-import com.example.BTL_CNPM.repository.UserRepository;
-import com.example.BTL_CNPM.service.AuthService;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-public class AuthServiceImpl implements AuthService {
-
-    private UserRepository userRepository;
-
-    public AuthServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public boolean login(LoginRequest requestObject) {
-        Optional<User> userOptional = userRepository.findByUsername(requestObject.getUsername());
-        return userOptional.isPresent() && userOptional.get().getPassword().equals(requestObject.getPassword());
-    }
-
-    public boolean register(User user) {
-
-    }
-}
+//package com.example.BTL_CNPM.service.impl;
+//
+//import com.example.BTL_CNPM.model.LoginRequest;
+//import com.example.BTL_CNPM.model.User;
+//import com.example.BTL_CNPM.repository.UserRepository;
+//import com.example.BTL_CNPM.service.AuthService;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//public class AuthServiceImpl implements AuthService {
+//
+//    private UserRepository userRepository;
+//
+//    public AuthServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public boolean login(LoginRequest requestObject) {
+//        Optional<User> userOptional = userRepository.findByUsername(requestObject.getUsername());
+//        return userOptional.isPresent() && userOptional.get().getPassword().equals(requestObject.getPassword());
+//    }
+//
+//    public boolean register(User user) {
+//
+//    }
+//}
