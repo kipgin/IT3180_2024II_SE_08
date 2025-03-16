8322b6013163c535c12d05a8adb62e43a9abbd59
kipgin <ductamhyntt@gmail.com>
2025-03-16 23:06:27 +0700

    lan thu ba

---
+++ src/test/java/com/example/BTL_CNPM/BtlCnpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BTL_CNPM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlCnpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
