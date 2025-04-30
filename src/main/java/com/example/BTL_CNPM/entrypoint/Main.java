6056fe0b943fe6a333c9232a5cfb04d46be3edd9
caohuythinh2005 <caohuythinh@gmail.com>
2025-05-01 00:47:43 +0700

    fix

--- src/main/java/com/example/BTL_CNPM/entrypoint/Main.java
+++ src/main/java/com/example/BTL_CNPM/entrypoint/Main.java
@@ -7,7 +7,7 @@ public class Main {
 //            // EzGmail.sendEmail("thinhhuycaotraistorm@gmail.com", "Test Email", "Đây là email test từ EzGmail!");
 //
 //            // Gửi email với tệp đính kèm
-//            List<File> attachments = Arrays.asList(new File("src/main/resources/test.pdf"), new File("src/main/resources/image.jpg"));
+//            List<File> attachments = Arrays.asList(new File("src/main/resources"), new File("src/main/resources/image.jpg"));
 //            EzGmail.sendEmailWithAttachment("thinhhuycaotraistorm@gmail.com", "Test với file đính kèm", "Đây là email test với file", attachments);
 //        } catch (Exception e) {
 //            e.printStackTrace();
