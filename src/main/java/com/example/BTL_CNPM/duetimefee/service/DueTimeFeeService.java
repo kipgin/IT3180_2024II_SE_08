95dbdac48eb97c8f8e09ac8321957edeb2b1f93f
kipgin <ductamhyntt@gmail.com>
2025-05-29 19:07:07 +0700

    Fix @Scheduled (with no parameters in the corresponding method)

--- src/main/java/com/example/BTL_CNPM/duetimefee/service/DueTimeFeeService.java
+++ src/main/java/com/example/BTL_CNPM/duetimefee/service/DueTimeFeeService.java
@@ -6,6 +6,7 @@ import com.example.BTL_CNPM.duetimefee.repository.DueTimeFeeRepository;
 import com.example.BTL_CNPM.feemanage.controller.FeeManageController;
 import com.example.BTL_CNPM.feemanage.model.FeeManage;
 import com.example.BTL_CNPM.feemanage.model.FeeSection;
+import com.example.BTL_CNPM.feemanage.repository.FeeManageRepository;
 import com.example.BTL_CNPM.feemanage.service.FeeManageService;
 import com.example.BTL_CNPM.gmail.model.users.UsersGmail;
 import com.example.BTL_CNPM.gmail.service.UsersGmailService;
@@ -22,6 +23,9 @@ public class DueTimeFeeService {
     private DueTimeFeeRepository dueTimeFeeRepository;
 
     @Autowired
+    private FeeManageRepository feeManageRepository;
+
+    @Autowired
     private FeeManageService feeManageService;
 
     @Autowired
@@ -29,6 +33,7 @@ public class DueTimeFeeService {
 
     @Autowired
     private FeeManageController feeManageController;
+
 
     public DueTimeFee findById(Integer id){
         return dueTimeFeeRepository.findById(id).orElse(null);
@@ -45,32 +50,36 @@ public class DueTimeFeeService {
 
     //cap nhat phi luc 0h ngay dau tien dau thang
     @Scheduled(cron ="0 0 0 1 * *")
-    public void updateFeePerMonth(Integer id){
+    public void updateFeePerMonth(){
         List<FeeManage> feeManages= feeManageService.findAll();
-        DueTimeFee dueTimeFee = findById(id);
+        List<DueTimeFee> dueTimeFees = findAll();
         LocalDateTime now = LocalDateTime.now();
-        if(dueTimeFee == null){
-            System.out.println("Cannot find the corresponding duetimefee.\n");
+        if(dueTimeFees == null){
+            System.out.println("Cannot find the corresponding duetimefees.\n");
             return;
         }
 
-        String name= dueTimeFee.getName();
-        for(FeeManage feeManage : feeManages){
-            if(feeManage.getFeeSections() == null){
-                continue;
-            }
-            List<FeeSection> feeSections = feeManage.getFeeSections();
-            for(FeeSection feeSection : feeSections){
-                if(feeSection.getName().equals(name)){
-                    feeManage.setTotalFee(feeManage.getTotalFee() + feeSection.getFeeName().getMoneyPerBlock() * feeSection.getBlockUsed());
-                    break;
-                }
+        for(DueTimeFee dueTimeFee : dueTimeFees) {
+            String name = dueTimeFee.getName();
 
+            for (FeeManage feeManage : feeManages) {
+                if (feeManage.getFeeSections() == null) {
+                    continue;
+                }
+                List<FeeSection> feeSections = feeManage.getFeeSections();
+                for (FeeSection feeSection : feeSections) {
+                    if (feeSection.getName().equals(name)) {
+                        feeManage.setTotalFee(feeManage.getTotalFee() + feeSection.getFeeName().getMoneyPerBlock() * feeSection.getBlockUsed());
+                        feeManageRepository.save(feeManage);
+                        break;
+                    }
+
+                }
             }
+            dueTimeFee.setDueTime(now.plusMonths(1));
+            dueTimeFee.setLastTime(now);
+            dueTimeFeeRepository.save(dueTimeFee);
         }
-        dueTimeFee.setDueTime(now.plusMonths(1));
-        dueTimeFee.setLastTime(now);
-        dueTimeFeeRepository.save(dueTimeFee);
     }
 
     public boolean update(DueTimeFee dueTimeFee){
